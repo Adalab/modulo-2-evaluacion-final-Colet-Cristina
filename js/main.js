af0931b23e592f7878c572a2472411193a3ee8bd
Cristina <tu-emailejemplo.com>
2025-11-12 13:38:42 +0100

    filter section

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@
 // SECCIÓN DE QUERY-SELECTOR
 
 const productsList = document.querySelector(".js_productsList");
+const filterInput = document.querySelector(".js_filterInput");
+const filterBtn = document.querySelector(".js_filterBtn");
 
 //SECCIÓN DE DATOS
 
-let products = []; // estan guardados los productos del servidor
+let products = [];
 
 // SECCIÓN DE FUNCIONES
 
@@ -32,7 +34,45 @@ function renderProducts() {
   productsList.innerHTML = html;
 }
 
+function renderFilteredProducts(filteredList) {
+  let html = "";
+  for (const product of filteredList) {
+    html += remderProductsList(product);
+  }
+  productsList.innerHTML = html;
+}
+
+// SECCIÓN DE FUNCIONES DE EVENTOS
+
+const handleFilterClick = (ev) => {
+  ev.preventDefault();
+  const nameFilter = filterInput.value;
+  console.log(nameFilter);
+
+  if (nameFilter === "") {
+    renderProducts(products);
+    return;
+  }
+
+  const filterProducts = products.filter((productObj) => {
+    return (
+      productObj.title.toLowerCase().includes(nameFilter) ||
+      productObj.category.toLowerCase().includes(nameFilter) ||
+      productObj.description.toLowerCase().includes(nameFilter)
+    );
+  });
+
+  renderFilteredProducts(filterProducts);
+};
+
+// SECCIÓN DE EVENTOS
+
+filterBtn.addEventListener("click", handleFilterClick);
+
+// SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA
+
 const dataInLS = JSON.parse(localStorage.getItem("productsBackup"));
+console.log("dataInSL", dataInLS);
 
 if (dataInLS === null) {
   fetch("https://fakestoreapi.com/products")

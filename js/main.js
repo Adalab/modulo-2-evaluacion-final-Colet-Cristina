8132bcf0cd9ffbae511f6596a4e0a88d585a2b6a
Cristina <tu-emailejemplo.com>
2025-11-12 03:28:16 +0100

    add fetch

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 "use strict";
 
+"use strict";
+
 // SECCIÓN DE QUERY-SELECTOR
-// Éstos son los elementos que nos traemos de la página HTML y usamos en el código
+
 const productsList = document.querySelector(".js_productsList");
 
+//SECCIÓN DE DATOS
+
+let products = [];
+
 // SECCIÓN DE FUNCIONES
-// Estos son funciones:
-//   - con código auxiliar
-//   - con código que usaremos en los eventos
-//   - para pintar (render) en la página.
 
 function remderProductsList(item) {
   const html = `
@@ -22,24 +24,28 @@ function remderProductsList(item) {
   </li>`;
   return html;
 }
+function renderProducts() {
+  let html = "";
+  for (const oneProductsList of products) {
+    html += remderProductsList(oneProductsList);
+  }
+
+  productsList.innerHTML = html;
+}
 
 // SECCIÓN DE EVENTOS
-// Estos son los eventos a los que reacciona la página
-// Los más comunes son: click (en botones, enlaces), input (en ídem) y submit (en form)
 
 // SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA
-// Este código se ejecutará cuando se carga la página
-// Lo más común es:
-//   - Pedir datos al servidor
-//   - Pintar (render) elementos en la página
-const item = {
-  title: " Mochila Fjallraven Foldsack No. 1, para portátiles de 15 pulgadas ",
-  price: 109.95,
-  category: " ropa de hombre ",
-  image: " https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png ",
-};
 
-const html = remderProductsList(item);
-productsList.innerHTML = html;
+fetch("https://fakestoreapi.com/products")
+  .then((res) => res.json())
+  .then((data) => {
+    products = data;
+    renderProducts();
+  })
+  .catch((error) => {
+    console.error("Error al cargar productos:", error);
+  });
 
+renderProducts(products);
 console.log("Página y JS cargados!");
